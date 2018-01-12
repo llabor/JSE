53ff2759a59a85018108b6c6e0b3d93029d014bf
Ezequiel Llarena Borges <ezequiel.formacion@gmail.com>
2018-01-12 17:11:01 +0100

    Update TestVisibilidad.java

--- superclass-subclass-protected/src/TestVisibilidad.java
+++ superclass-subclass-protected/src/TestVisibilidad.java
@@ -1,18 +1,17 @@
-
+/**
+ * @author Ezequiel Llarena Borges
+ * @version 1.0
+ * @since jan 2018 
+ * */
 public class TestVisibilidad {
-
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		Superclase sp = new Superclase(0, 2);
-		Subclase sc = new Subclase(0, 1, 2);
-		
+		Subclase sc = new Subclase(0, 1, 2);	
 //		sp.a = 1;
 //		sc.a = 0;
 //		sp.a = 0;
 //		sc.b = 0;
 //		sc.a = 0;
 		
-
 	}
-
 }
